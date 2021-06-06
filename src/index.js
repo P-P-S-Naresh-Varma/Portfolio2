f2c2ea0dc030e41ed593da5f8fa34753d7bbae50
Demon-stare <62430756+Demon-stare@users.noreply.github.com>
2021-06-06 18:32:07 +0530

    h]

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import "./styles.css";
 
 
+import { jsPDF } from "jspdf";
+
+// Default export is a4 paper, portrait, using millimeters for units
+
+var pdf = new jsPDF('p', 'pt', 'a4');
+
+    function onClick() {
+        window.html2canvas = html2canvas
+        const doc = document.getElementsByTagName('div')[0];
+
+        if (doc) {
+            console.log("div is ");
+            console.log(doc);
+            console.log("hellowww");
+
+
+
+            pdf.html(document.getElementById('doc'), {
+                callback: function (pdf) {
+                    pdf.save('DOC.pdf');
+                }
+            })
+       }
+     }
+
+var element = document.getElementById("D");
+element.addEventListener("click", onClick);
