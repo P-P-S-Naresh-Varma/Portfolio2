07e1a61f6572d8b4176a4c80e9cb636ae7b4a47c
Demon-stare <62430756+Demon-stare@users.noreply.github.com>
2022-09-14 15:31:18 +0530

    new fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import "./styles.css";
 
 document.getElementById("linkedinid").addEventListener("click", openLinkedin);
 document.getElementById("viewresume").addEventListener("click", viewresume);
